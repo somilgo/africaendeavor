1b82e65e4c4f8ca501f9ba239094353da9e9412e
somilgo <somilgovani@gmail.com>
2016-05-22 16:21:39 -0400

    masonry

--- africaendeavor/urls.py
+++ africaendeavor/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls import handler404
 from django.conf import settings
-from django.conf.urls.static import staticn od
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
