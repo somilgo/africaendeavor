d56d679c76f76ca925e56b2a6846197284692c47
somilgo <somilgovani@gmail.com>
2016-05-22 15:56:35 -0400

    added masonry

--- africaendeavor/settings/production.py
+++ africaendeavor/settings/production.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = ['*']
  
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # STATIC_ROOT = 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
