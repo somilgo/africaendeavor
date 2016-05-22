92c3875ffffd3c51e623cb5ad28e933e52f2a845
somilgo <somilgovani@gmail.com>
2016-05-22 15:46:12 -0400

    added masonry

--- africaendeavor/settings/base.py
+++ africaendeavor/settings/base.py
@@ -109,7 +109,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+PROJECT_ROOT=BASE_DIR
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
